c0a9d43c1b77eab95e41fa96cad4178efba300e5
Jing Wu <jingwusince01@gmail.com>
2025-11-27 18:46:52 +0800

    Change local_rank arg

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def get_args_parser():
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
     parser.add_argument('--rank', default=0, type=int,
                         help='number of distributed processes')
-    parser.add_argument("--local_rank", type=int, help='local rank for DistributedDataParallel')
+    parser.add_argument("--local_rank", "--local-rank", type=int, help='local rank for DistributedDataParallel')
     parser.add_argument('--amp', action='store_true',
                         help="Train with mixed precision")
     
