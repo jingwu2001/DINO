c705598470def8c8af889066e99aaa42c3905593
Jing Wu <jingwusince01@gmail.com>
2025-12-09 19:22:43 +0800

    :

--- scripts/inference_repl.sh
+++ scripts/inference_repl.sh
@@ -1,7 +1,7 @@
 # Inference command for predictions_15.txt
 python inference_15.py \
     --config_file config/DINO/DINO_valve_5scale.py \
-    --checkpoint_path chckpoints_for_replication/checkpoint_5scale_24epochs.pth \
+    --checkpoint_path checkpoints/checkpoint_5scale_24epochs.pth \
     --input_folder data/testing_image \
     --output_file $1 \
     --confidence_threshold 0.15 \
@@ -10,7 +10,7 @@ python inference_15.py \
 
 python inference_4scale_15.py \
     --config_file config/DINO/DINO_valve_4scale.py \
-    --checkpoint_path chckpoints_for_replication/checkpoint_4scale_24epochs.pth \
+    --checkpoint_path checkpoints/checkpoint_4scale_24epochs.pth \
     --input_folder data/testing_image \
     --output_file $2 \
     --confidence_threshold 0.15 \
